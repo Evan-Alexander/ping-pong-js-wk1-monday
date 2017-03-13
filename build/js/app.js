53c814f7c85bb65aa218f331c7e82a63168177a6
Ash Laidlaw and Jason Brown <ash@>
2017-03-13 10:28:57 -0700

    Add weather API

--- build/js/app.js
+++ build/js/app.js
@@ -49,4 +49,16 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
+var apiKey = "f96901d2ce6723952ba93cceb4fc0e1c";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The weather in " + city + " is " + (response.main.temp * 9/5 - 459.67).toFixed(2) + ".");
+    });
+  });
+});
+
 },{"./../js/pingpong.js":1}]},{},[2]);
